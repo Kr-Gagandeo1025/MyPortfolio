d7f41440f8ad3cabc6f771838bd6f0bce5a4f2d8
Kr-Gagandeo1025 <88143827+Kr-Gagandeo1025@users.noreply.github.com>
2024-03-21 00:49:22 +0530

    deployment fix

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <div id="collab" className="flex justify-evenly items-center flex-col lg:flex-row w-screen h-screen border-t-2">
         <div className="flex flex-col lg:h-[70%]">
             <div className='flex flex-col lg:ml-12 ml-14'>
-                <span className='text-6xl lg:text-9xl'><span className="text-red-500">L</span>et's</span>
+                <span className='text-6xl lg:text-9xl'><span className="text-red-500">L</span>et&apos;s</span>
                 <span className='text-6xl lg:text-9xl'>Connect.</span>
             </div>
             <span className="text-2xl lg:ml-12 mt-12 z-50 flex gap-4 items-center w-fit"><Mail/><a href="mailto:kumargagandeo9@gmail.com">kumargagandeo9@gmail.com</a></span>
@@ -19,16 +19,16 @@ const Contact = () => {
         </div>
         <div className='flex flex-col justify-between lg:justify-end lg:h-[70%]'>
             <h1 className='text-3xl lg:text-5xl'>Resume</h1>
-            <p className="flex-wrap w-[300px]">See my work history and what I've achieved in a quick and easy resume.</p>
+            <p className="flex-wrap w-[300px]">See my work history and what I&apos;ve achieved in a quick and easy resume.</p>
         </div>
         <div className="flex flex-col justify-between lg:gap-5 lg:h-[70%]">
             <div>
                 <h1 className='text-3xl lg:text-5xl'>Work</h1>
-                <p className="flex-wrap w-[300px]">Here's a curated selection showcasing my expertise and the achieved results.</p>
+                <p className="flex-wrap w-[300px]">Here&apos;s a curated selection showcasing my expertise and the achieved results.</p>
             </div>
             <div>
                 <h1 className='text-3xl lg:text-5xl'>Skills</h1>
-                <p className="flex-wrap w-[300px]">Check out the things I'm good at, from building websites to design and more.</p>
+                <p className="flex-wrap w-[300px]">Check out the things I&apos;m good at, from building websites to design and more.</p>
             </div>
             <div>
                 <h1 className='text-3xl lg:text-5xl'>Instagram</h1>
@@ -37,7 +37,7 @@ const Contact = () => {
         </div>
         <div className="flex justify-between flex-col">
                 <h1 className='text-3xl lg:text-5xl'>Experience</h1>
-                <p className="flex-wrap w-[300px]">Check out things I'm good at, from building websites to design and more.</p>
+                <p className="flex-wrap w-[300px]">Check out things I&apos;m good at, from building websites to design and more.</p>
         </div>
     </div>
   )
